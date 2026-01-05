4f8ad7f0106025d13d58c1547ba17b55086e515a
iankressin <ianguimaraes31@gmail.com>
2026-01-05 11:36:45 -0300

    fix: metadata

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 import { dev } from '$app/environment';
 
-export const title = 'Ian K. Guimarães';
-export const description = 'SvelteKit blog for poets';
+export const title = 'Dan Quirk';
+export const description = 'Thoughts on products, humans, and how they interact';
 //export const url = dev ? 'http://localhost:5173/' : 'https://iankguimaraes.com'
 export const url = 'http://localhost:5173/';
