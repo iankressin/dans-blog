5014591f003e01e95398a9b7f5228925468ae812
iankressin <ianguimaraes31@gmail.com>
2026-01-05 12:12:15 -0300

    fix: bg color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 	import Header from '$lib/components/Header.svelte';
 </script>
 
-<div class="flex justify-center bg-dim-1 dark:bg-[#232a2e] text-white dark:text-[#ddd8be] h-full min-h-screen pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)]">
+<div class="flex justify-center bg-dim-1 dark:bg-[#232a2e] text-white dark:text-[#ddd8be] min-h-screen w-full pt-[env(safe-area-inset-top)] pb-[env(safe-area-inset-bottom)]">
 	<div class="h-full w-full laptop:max-w-4xl py-8 px-8 laptop:px-20 flex flex-col gap-16">
 		<Header />
 
